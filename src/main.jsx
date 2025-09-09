b02384543c4a130dc6992aced2decaab8be0a724
endayshebocah <endayshebocah@gmail.com>
2025-09-09 16:15:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Baris ini adalah kunci untuk memuat semua style Tailwind CSS Anda
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
